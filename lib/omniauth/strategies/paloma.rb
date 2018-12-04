44616384b355f2f0c88183893691b11359326fdc
Lenart Rudel <lenart.rudel@gmail.com>
2018-12-04 09:25:29 +0100

    Use ENV for oAuth endpoint

--- lib/omniauth/strategies/paloma.rb
+++ lib/omniauth/strategies/paloma.rb
@@ -3,15 +3,17 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Paloma < OmniAuth::Strategies::OAuth2
+      URL = ENV["PALOMA_OAUTH_ENDPOINT"].freeze || "https://sso.bauwerk-parkett.com".freeze
+
       # Give your strategy a name.
       option :name, "paloma"
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
       option :client_options, {
-        site: "https://baum-stage.paloma.one/",
-        authorize_url: "https://baum-stage.paloma.one/users/oauth/authorize",
-        token_url: "https://baum-stage.paloma.one/users/oauth/token"
+        site: URL,
+        authorize_url: "#{URL}/users/oauth/authorize",
+        token_url: "#{URL}/users/oauth/token"
       }
 
       # These are called after authentication has succeeded. If
